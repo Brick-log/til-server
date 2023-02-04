766ec9af99050416d772e41b73c8aadeee1ea3c4
dduck00 <ejr075@gmail.com>
2023-02-04 22:36:52 +0900

    feat: implement domain object (#8)
    * feat: implement domain object
    
    * add: main class property

--- src/api/src/main/kotlin/com/tenmm/tilserver/category/adapter/inbound/rest/GetCategoryController.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/category/adapter/inbound/rest/GetCategoryController.kt
@@ -2,6 +2,8 @@ package com.tenmm.tilserver.category.adapter.inbound.rest
 
 import com.tenmm.tilserver.category.adapter.inbound.rest.model.GetCategoriesResponse
 import com.tenmm.tilserver.category.domain.Category
+import com.tenmm.tilserver.common.domain.Identifier
+import java.util.UUID
 import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,7 +17,7 @@ class GetCategoryController {
         return GetCategoriesResponse(
             categories = listOf(
                 Category(
-                    identifier = RandomStringUtils.random(100),
+                    identifier = Identifier(UUID.randomUUID().toString()),
                     name = RandomStringUtils.random(100),
                 )
             )
