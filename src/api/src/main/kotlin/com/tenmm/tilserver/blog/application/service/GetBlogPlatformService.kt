f5a81fa36ec3c8313302bcd808fcf78d52c90ce1
ktj1997 <rlaxowns7916@gmail.com>
2023-02-07 17:21:02 +0900

    fix: Build Fail 수정

--- src/api/src/main/kotlin/com/tenmm/tilserver/blog/application/service/GetBlogPlatformService.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/blog/application/service/GetBlogPlatformService.kt
@@ -2,10 +2,11 @@ package com.tenmm.tilserver.blog.application.service
 
 import com.tenmm.tilserver.blog.application.inbound.GetBlogPlatformUseCase
 import com.tenmm.tilserver.blog.domain.BlogPlatformType
+import com.tenmm.tilserver.common.domain.Url
 import org.springframework.stereotype.Service
 
 @Service
 class GetBlogPlatformService : GetBlogPlatformUseCase {
     override fun getAll() = BlogPlatformType.getAll()
-    override fun getByUrl(url: String): BlogPlatformType = BlogPlatformType.get(url)
+    override fun getByUrl(url: Url): BlogPlatformType = BlogPlatformType.get(url.value)
 }
