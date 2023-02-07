4705903105436658e06a4251e6003d5c917941d6
ktj1997 <rlaxowns7916@gmail.com>
2023-02-07 17:08:08 +0900

    fix: 리뷰 반영

--- src/api/src/main/kotlin/com/tenmm/tilserver/blog/application/inbound/GetBlogPlatformUseCase.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/blog/application/inbound/GetBlogPlatformUseCase.kt
@@ -1,8 +1,9 @@
 package com.tenmm.tilserver.blog.application.inbound
 
 import com.tenmm.tilserver.blog.domain.BlogPlatformType
+import com.tenmm.tilserver.common.domain.Url
 
 interface GetBlogPlatformUseCase {
     fun getAll(): List<BlogPlatformType>
-    fun getByUrl(url: String): BlogPlatformType
+    fun getByUrl(url: Url): BlogPlatformType
 }
