02023c48957961dff498e5591c0cab18d5cf084b
TaeJun Kim <rlaxowns7916@gmail.com>
2023-02-08 21:27:44 +0900

    Feature/define blog usecase (#10)

---
+++ src/api/src/main/kotlin/com/tenmm/tilserver/blog/application/inbound/model/SaveBlogCommand.kt
@@ -0,0 +1,9 @@
+package com.tenmm.tilserver.blog.application.inbound.model
+
+import com.tenmm.tilserver.common.domain.Identifier
+import com.tenmm.tilserver.common.domain.Url
+
+data class SaveBlogCommand(
+    val userIdentifier: Identifier,
+    val url: Url
+)
---
+++ src/api/src/test/kotlin/com/tenmm/tilserver/blog/domain/BlogPlatformTypeTest.kt
@@ -0,0 +1,88 @@
+package com.tenmm.tilserver.blog.domain
+
+import com.tenmm.tilserver.common.domain.Url
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+/**
+ * Velog의 경우, velog.io/@username/title 의 형태를 가진다.
+ */
+class TestVelog {
+    @Test
+    fun `Test Velog (Standard)`() {
+        val url = Url("https://velog.io/@username/post")
+        assertEquals(BlogPlatformType.VELOG, BlogPlatformType.get(url))
+    }
+}
+
+/**
+ * Medium의 경우, medium.com이 다양한 곳에 위치 할 수 있다.
+ *
+ * Case 1: medium.com/@username/title
+ * Case 2: [prefix].meduim.com/title
+ * Case 3: medium.com/[postfix]/title
+ * Case 4: medium.com이 아예 들어가지 않음 (식별 불가)
+ */
+class TestMeduim {
+    @Test
+    fun `Test Meduim (Standard)`() {
+        val url = Url("https://medium.com/@username/title")
+        assertEquals(BlogPlatformType.MEDIUM, BlogPlatformType.get(url))
+    }
+
+    @Test
+    fun `Test Meduim (with prefix)`() {
+        val url = Url("https://jacquelinedooley.medium.com/title")
+        assertEquals(BlogPlatformType.MEDIUM, BlogPlatformType.get(url))
+    }
+
+    @Test
+    fun `Test Meduim (with postfix)`() {
+        val url = Url("https://medium.com/postfix/title")
+        assertEquals(BlogPlatformType.MEDIUM, BlogPlatformType.get(url))
+    }
+    @Test
+    fun `Test Meduim (not contain meduim URL)`() {
+        /**
+         * Sample Real URL
+         */
+        val url = Url("https://betterprogramming.pub/context-over-task-lists-2e8912d7df61")
+
+        assertEquals(BlogPlatformType.ETC, BlogPlatformType.get(url))
+    }
+}
+
+/**
+ * Naver 의 경우, blog.naver.com으로 시작한다.
+ * blog.naver.com/nickname/[postIdentifier]
+ *
+ */
+class TestNaver {
+    @Test
+    fun `Test naver (Standard)`() {
+        val url = Url("https://blog.naver.com/nickname/223302012")
+        assertEquals(BlogPlatformType.NAVER, BlogPlatformType.get(url))
+    }
+}
+
+/**
+ * Tistory 의 경우, 다양한 형태를 가진다.
+ * Case 1: [prefix]].tistory.com/[postIdentifier]의 형태를 가진다.
+ * Case 2: tistory.com이 아예 들어가지 않음 (식별 불가)
+ */
+class TestTisotry {
+    @Test
+    fun `Test Tistory (Standard)`() {
+        val url = Url("https://prefix.tistory.com/nickname/223302012")
+        assertEquals(BlogPlatformType.TISTORY, BlogPlatformType.get(url))
+    }
+
+    @Test
+    fun `Test Tistory (not contain tistory URL)`() {
+        /**
+         * Sample Real URL
+         */
+        val url = Url("https://blog.lgcns.com/")
+        assertEquals(BlogPlatformType.ETC, BlogPlatformType.get(url))
+    }
+}
