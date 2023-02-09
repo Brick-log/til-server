6281af7ec08ad4e2e4e24648e326cec5a7ed9c08
dduck00 <ejr075@gmail.com>
2023-02-09 23:22:31 +0900

    define: define more post usecase (#13)

--- src/api/src/main/kotlin/com/tenmm/tilserver/post/application/inbound/GetRecommendedPostUseCase.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/post/application/inbound/GetRecommendedPostUseCase.kt
@@ -4,5 +4,6 @@ import com.tenmm.tilserver.common.domain.Identifier
 import com.tenmm.tilserver.post.application.inbound.model.GetPostListResult
 
 interface GetRecommendedPostUseCase {
+    fun getRecommendedPostListRandom(): GetPostListResult
     fun getRecommendedPostListByCategory(categoryIdentifier: Identifier): GetPostListResult
 }
