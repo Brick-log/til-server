11767e321cd50a20b06ca8ab561abf6331b47bd2
dduck00 <ejr075@gmail.com>
2023-02-19 01:55:37 +0900

    feat: add grpc config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,8 @@ rootProject.name = "til-server"
 includeProject(":api", "src/api")
 includeProject(":crawler", "src/crawler")
 
+includeProject(":protocol", "libs/protocol")
+
 fun includeProject(name: String, projectPath: String) {
     include(name)
     project(name).setProjectDir(file(projectPath))
