696c6794e98ec7f906cf0da9b2ab4280164dbf47
dduck00 <ejr075@gmail.com>
2023-02-26 23:54:59 +0900

    feat: impl mock api post, user api

--- src/api/src/main/kotlin/com/tenmm/tilserver/user/adapter/inbound/GetUserMyProfileController.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/user/adapter/inbound/GetUserMyProfileController.kt
@@ -1,8 +1,10 @@
 package com.tenmm.tilserver.user.adapter.inbound
 
 import com.tenmm.tilserver.common.domain.Identifier
+import com.tenmm.tilserver.common.domain.Url
 import com.tenmm.tilserver.user.adapter.inbound.model.GetUserProfileResponse
 import com.tenmm.tilserver.user.application.inbound.GetUserUseCase
+import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,7 +16,14 @@ class GetUserMyProfileController(
 ) {
     @GetMapping
     fun getUserMyProfile(): GetUserProfileResponse {
-        val user = getUserUseCase.getByIdentifier(Identifier.generate())
-        return GetUserProfileResponse.fromUser(user)
+//        val user = getUserUseCase.getByIdentifier(Identifier.generate())
+//        return GetUserProfileResponse.fromUser(user)
+        return GetUserProfileResponse(
+            name = RandomStringUtils.random(10),
+            profileImgSrc = Url("https://www.naver.com/"),
+            introduction = RandomStringUtils.random(10),
+            categoryId = Identifier.generate(),
+            isAuthorized = false
+        )
     }
 }
--- src/api/src/main/kotlin/com/tenmm/tilserver/user/adapter/inbound/GetUserProfileController.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/user/adapter/inbound/GetUserProfileController.kt
@@ -1,7 +1,10 @@
 package com.tenmm.tilserver.user.adapter.inbound
 
+import com.tenmm.tilserver.common.domain.Identifier
+import com.tenmm.tilserver.common.domain.Url
 import com.tenmm.tilserver.user.adapter.inbound.model.GetUserProfileResponse
 import com.tenmm.tilserver.user.application.inbound.GetUserUseCase
+import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,7 +19,14 @@ class GetUserProfileController(
     fun getUserProfile(
         @PathVariable path: String,
     ): GetUserProfileResponse {
-        val user = getUserUseCase.getByPath(path)
-        return GetUserProfileResponse.fromUser(user)
+//        val user = getUserUseCase.getByPath(path)
+//        return GetUserProfileResponse.fromUser(user)
+        return GetUserProfileResponse(
+            name = RandomStringUtils.random(10),
+            profileImgSrc = Url("https://www.naver.com/"),
+            introduction = RandomStringUtils.random(10),
+            categoryId = Identifier.generate(),
+            isAuthorized = false
+        )
     }
 }
