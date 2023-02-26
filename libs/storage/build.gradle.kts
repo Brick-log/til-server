f2d04b263cb298578f5c76f19cb1a104182749bd
계희준 <gyehuijun@gyehuijun-ui-MacBookPro.local>
2023-02-26 20:14:57 +0900

    Modified: redis dependency 추가

--- libs/storage/build.gradle.kts
+++ libs/storage/build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
 dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
     api("org.springframework.boot:spring-boot-starter-data-jpa")
+    api("org.springframework.boot:spring-boot-starter-data-redis")
 }
