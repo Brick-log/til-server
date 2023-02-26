5ec7ee1e20e6d322c32c753ee5ef082f1e2659bc
계희준 <gyehuijun@gyehuijun-ui-MacBookPro.local>
2023-02-26 19:58:06 +0900

    Added: redis 사용 설정

--- libs/storage/src/main/kotlin/com/tenmm/tilserver/outbound/persistence/repository/DraftRepository.kt
+++ libs/storage/src/main/kotlin/com/tenmm/tilserver/outbound/persistence/repository/DraftRepository.kt
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository
 @Repository
 interface DraftRepository : JpaRepository<DraftEntity, Long> {
     fun findByUserIdentifier(userIdentifier: String): DraftEntity?
-    fun sync(draftEntity: DraftEntity)
 }
--- src/api/src/main/kotlin/com/tenmm/tilserver/draft/adapter/outbound/SyncDraftAdapter.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/draft/adapter/outbound/SyncDraftAdapter.kt
@@ -2,16 +2,16 @@ package com.tenmm.tilserver.draft.adapter.outbound
 
 import com.tenmm.tilserver.common.domain.Identifier
 import com.tenmm.tilserver.draft.application.outbound.SyncDraftPort
-import com.tenmm.tilserver.outbound.persistence.repository.DraftRepository
-import com.tenmm.tilserver.outbound.persistence.entity.DraftEntity
+import com.tenmm.tilserver.outbound.persistence.repository.DraftSyncRepository
+import com.tenmm.tilserver.outbound.persistence.entity.DraftSyncEntity
 import java.time.LocalDateTime
 
 class SyncDraftAdapter(
-    private val draftSyncRepository: DraftRepository
+    private val draftSyncRepository: DraftSyncRepository
 ) : SyncDraftPort {
     override fun syncDraft(userIdentifier: Identifier, data: String) {
         draftSyncRepository.sync(
-            DraftEntity(
+            DraftSyncEntity(
                 userIdentifier = userIdentifier.value,
                 data = data,
                 updatedAt = LocalDateTime.now()
