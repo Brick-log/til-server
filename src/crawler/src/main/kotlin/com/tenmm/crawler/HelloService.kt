9652acf676c2dd34c9d9c3a083b9b3e6d8c88bc0
dduck00 <ejr075@gmail.com>
2023-02-26 22:36:49 +0900

    feat: impl grpc (#28)

--- src/crawler/src/main/kotlin/com/tenmm/crawler/HelloService.kt
+++ src/crawler/src/main/kotlin/com/tenmm/crawler/HelloService.kt
@@ -3,9 +3,9 @@ package com.tenmm.crawler
 import io.grpc.examples.helloworld.GreeterGrpcKt
 import io.grpc.examples.helloworld.HelloReply
 import io.grpc.examples.helloworld.HelloRequest
-import net.devh.boot.grpc.server.service.GrpcService
+import org.springframework.stereotype.Component
 
-@GrpcService
+@Component
 class HelloService : GreeterGrpcKt.GreeterCoroutineImplBase() {
     override suspend fun sayHello(request: HelloRequest): HelloReply {
         return HelloReply.newBuilder()
