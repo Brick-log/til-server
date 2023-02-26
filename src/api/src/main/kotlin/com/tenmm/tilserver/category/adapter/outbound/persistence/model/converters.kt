3b568ed80a0a125bc03ac9b629e85b9c61a512e2
계희준 <gyehuijun@gyehuijun-ui-MacBookPro.local>
2023-02-26 18:10:33 +0900

    Modified: lint 오류 수정

--- src/api/src/main/kotlin/com/tenmm/tilserver/category/adapter/outbound/persistence/model/converters.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/category/adapter/outbound/persistence/model/converters.kt
@@ -1,11 +1,10 @@
 package com.tenmm.tilserver.category.adapter.outbound.persistence.model
 
 import com.tenmm.tilserver.category.domain.Category
-import com.tenmm.tilserver.common.domain.Identifier
 
 fun CategoryEntity.toDomain(): Category {
     return Category(
-        identifier = identifier.value, 
+        identifier = identifier.value,
         name = name
     )
 }
