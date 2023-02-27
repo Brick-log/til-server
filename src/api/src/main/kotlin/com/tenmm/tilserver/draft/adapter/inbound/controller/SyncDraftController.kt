30c332ec3f3c3d4d558186b93f7732f47cb70b4b
계희준 <gyehuijun@gyehuijun-ui-MacBookPro.local>
2023-02-27 19:44:41 +0900

    Modified: Mockdata input/output

--- src/api/src/main/kotlin/com/tenmm/tilserver/draft/adapter/inbound/controller/SyncDraftController.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/draft/adapter/inbound/controller/SyncDraftController.kt
@@ -6,14 +6,24 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+// 추후 redis 사용 시 SyncDraftUseCase로 변경
+import com.tenmm.tilserver.draft.application.inbound.SaveDraftUseCase
+import com.tenmm.tilserver.common.domain.Identifier
 
 @RestController
 @RequestMapping("/v1/my/draft")
-class SyncDraftController {
+class SyncDraftController(
+    private val saveDraftUseCase: SaveDraftUseCase,
+) {
     @PutMapping("/sync")
     fun syncDraft(
         @RequestBody syncDraftRequest: SyncDraftRequest,
     ): SyncDraftResponse {
+        saveDraftUseCase.save(
+            Identifier.generate(),
+            syncDraftRequest.data
+        )
+
         return SyncDraftResponse(true)
     }
 }
