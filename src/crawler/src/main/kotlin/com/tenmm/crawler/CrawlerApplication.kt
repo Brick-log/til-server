d09579b5b180769609cfde9fe10457e887f0f342
계희준 <gyehuijun@gyehuijun-ui-MacBookPro.local>
2023-03-12 18:02:20 +0900

    Modified: crawler scanBasePackages에 com.tenmm.tilserver추가

--- src/crawler/src/main/kotlin/com/tenmm/crawler/CrawlerApplication.kt
+++ src/crawler/src/main/kotlin/com/tenmm/crawler/CrawlerApplication.kt
@@ -5,7 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @ConfigurationPropertiesScan
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = ["com.tenmm.tilserver"]
+)
 class CrawlerApplication
 
 fun main(args: Array<String>) {
