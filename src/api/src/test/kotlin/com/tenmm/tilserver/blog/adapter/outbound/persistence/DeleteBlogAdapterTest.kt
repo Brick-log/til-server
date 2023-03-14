16beeeba69c33d886c8d9627451710681d4245a4
ktj1997 <rlaxowns7916@gmail.com>
2023-03-14 22:29:42 +0900

    fix: lint

--- src/api/src/test/kotlin/com/tenmm/tilserver/blog/adapter/outbound/persistence/DeleteBlogAdapterTest.kt
+++ src/api/src/test/kotlin/com/tenmm/tilserver/blog/adapter/outbound/persistence/DeleteBlogAdapterTest.kt
@@ -1,7 +1,6 @@
 package com.tenmm.tilserver.blog.adapter.outbound.persistence
 
 import io.mockk.junit5.MockKExtension
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.extension.ExtendWith
 
