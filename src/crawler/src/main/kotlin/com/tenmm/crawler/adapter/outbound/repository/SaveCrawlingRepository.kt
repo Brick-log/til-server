2442145705c88c36273b0fe44d92705df61f560f
계희준 <gyehuijun@gyehuijun-ui-MacBookPro.local>
2023-04-02 16:52:33 +0900

    Modified: Repositotry 상속

---
+++ src/crawler/src/main/kotlin/com/tenmm/crawler/adapter/outbound/repository/SaveCrawlingRepository.kt
@@ -0,0 +1,7 @@
+package com.tenmm.crawler.adapter.outbound.repository
+
+import com.tenmm.tilserver.outbound.persistence.repository.ParsedPostRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface SaveCrawlingRepository : ParsedPostRepository
