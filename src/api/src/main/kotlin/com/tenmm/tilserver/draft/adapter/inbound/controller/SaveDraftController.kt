e3b0367311ca3db98d6694bf7a85f62639505ebf
dduck00 <ejr075@gmail.com>
2023-04-06 22:19:13 +0900

    fix: modify ktlint

--- src/api/src/main/kotlin/com/tenmm/tilserver/draft/adapter/inbound/controller/SaveDraftController.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/draft/adapter/inbound/controller/SaveDraftController.kt
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import com.tenmm.tilserver.common.domain.Identifier
 
 @RestController
 @RequestMapping("/v1/my/draft")
