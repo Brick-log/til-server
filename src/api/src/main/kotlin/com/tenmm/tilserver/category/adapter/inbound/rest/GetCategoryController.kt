2cb06752200b239e29cc5226242ead0be7b84227
dduck00 <ejr075@gmail.com>
2023-04-06 21:45:21 +0900

    feat: impl category fin

--- src/api/src/main/kotlin/com/tenmm/tilserver/category/adapter/inbound/rest/GetCategoryController.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/category/adapter/inbound/rest/GetCategoryController.kt
@@ -1,16 +1,14 @@
 package com.tenmm.tilserver.category.adapter.inbound.rest
 
-import com.tenmm.tilserver.category.adapter.inbound.rest.model.CategoryResult
 import com.tenmm.tilserver.category.adapter.inbound.rest.model.GetCategoriesResponse
+import com.tenmm.tilserver.category.adapter.inbound.rest.model.toResult
+import com.tenmm.tilserver.category.application.inbound.GetCategoryUseCase
 import com.tenmm.tilserver.common.adapter.inbound.rest.model.ErrorResponse
-import com.tenmm.tilserver.common.domain.Identifier
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
-import java.util.UUID
-import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -18,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @Tag(name = "Category")
 @RequestMapping("/v1/categories")
-class GetCategoryController {
+class GetCategoryController(
+    private val getCategoryUseCase: GetCategoryUseCase,
+) {
     @GetMapping
     @Operation(
         summary = "전체 카테고리 가져오기",
@@ -35,21 +35,6 @@ class GetCategoryController {
         ]
     )
     fun getCategories(): GetCategoriesResponse {
-        return GetCategoriesResponse(
-            categories = listOf(
-                CategoryResult(
-                    identifier = Identifier(UUID.randomUUID().toString()).toString(),
-                    name = RandomStringUtils.randomAlphabetic(10),
-                ),
-                CategoryResult(
-                    identifier = Identifier(UUID.randomUUID().toString()).toString(),
-                    name = RandomStringUtils.randomAlphabetic(10),
-                ),
-                CategoryResult(
-                    identifier = Identifier(UUID.randomUUID().toString()).toString(),
-                    name = RandomStringUtils.randomAlphabetic(10),
-                )
-            )
-        )
+        return GetCategoriesResponse(getCategoryUseCase.getAll().map { it.toResult() })
     }
 }
--- src/api/src/main/kotlin/com/tenmm/tilserver/category/application/service/GetCategoryService.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/category/application/service/GetCategoryService.kt
@@ -4,7 +4,6 @@ import com.tenmm.tilserver.category.application.inbound.GetCategoryUseCase
 import com.tenmm.tilserver.category.application.outbound.GetCategoryPort
 import com.tenmm.tilserver.category.domain.Category
 import com.tenmm.tilserver.common.domain.Identifier
-import com.tenmm.tilserver.common.domain.NotFoundException
 import org.springframework.stereotype.Service
 
 @Service
@@ -15,7 +14,7 @@ class GetCategoryService(
         return getCategoryPort.getAll()
     }
 
-    override fun getByIdentifier(identifier: Identifier): Category {
-        return getCategoryPort.getByIdentifier(identifier) ?: throw NotFoundException("No category - $identifier")
+    override fun getByIdentifier(identifier: Identifier): Category? {
+        return getCategoryPort.getByIdentifier(identifier)
     }
 }
