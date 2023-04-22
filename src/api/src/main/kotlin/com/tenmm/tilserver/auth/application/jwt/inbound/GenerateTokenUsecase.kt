26039df1f3d5a7d221a0e40251c9c8cb475d5f39
dduck00 <ejr075@gmail.com>
2023-04-22 15:41:42 +0900

    Revert "Feature/refresh token"

--- src/api/src/main/kotlin/com/tenmm/tilserver/auth/application/jwt/inbound/GenerateTokenUsecase.kt
+++
@@ -1,7 +0,0 @@
-package com.tenmm.tilserver.auth.application.jwt.inbound
-
-import com.tenmm.tilserver.auth.adapter.inboud.model.TokenResModel
-
-interface GenerateTokenUsecase {
-    fun generate(userIdentifier: String): TokenResModel
-}
--- src/api/src/main/kotlin/com/tenmm/tilserver/auth/application/jwt/inbound/VerifyRefreshTokenUsecase.kt
+++
@@ -1,5 +0,0 @@
-package com.tenmm.tilserver.auth.application.jwt.inbound
-
-interface VerifyRefreshTokenUsecase {
-    fun verify(accessToken: String, refreshToken: String): Boolean
-}
