1c1a059e9b8c7deea66d160c06beba67cdf7108a
dduck00 <ejr075@gmail.com>
2023-05-09 23:22:27 +0900

    modify github workflow support to deploy crawler

--- src/crawler/build.gradle.kts
+++ src/crawler/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     kotlin("jvm")
 }
@@ -10,4 +12,12 @@ dependencies {
     implementation("io.grpc:grpc-protobuf:1.53.0")
     implementation("com.google.protobuf:protobuf-kotlin:3.22.0")
     implementation("org.jsoup:jsoup:1.15.3")
+}
+
+tasks.named<BootJar>("bootJar") {
+    archiveFileName.set("crawler.jar")
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
 }
