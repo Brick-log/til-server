5c738f610d836b9c1ce964ff7e5cf65b0c980142
ktj1997 <rlaxowns7916@gmail.com>
2023-06-26 00:16:31 +0900

    fix: RefreshToken Header 정보 삭제

--- src/api/src/main/kotlin/com/tenmm/tilserver/security/application/service/RefreshTokenService.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/security/application/service/RefreshTokenService.kt
@@ -3,9 +3,11 @@ package com.tenmm.tilserver.security.application.service
 import com.tenmm.tilserver.common.domain.UnAuthorizedException
 import com.tenmm.tilserver.security.application.inbound.GenerateTokenUseCase
 import com.tenmm.tilserver.security.application.inbound.RefreshTokenUseCase
+import com.tenmm.tilserver.security.application.inbound.ResolveTokenUseCase
 import com.tenmm.tilserver.security.application.outbound.DeleteRefreshTokenPort
 import com.tenmm.tilserver.security.application.outbound.GetRefreshTokenPort
 import com.tenmm.tilserver.security.domain.SecurityToken
+import com.tenmm.tilserver.security.domain.SecurityTokenType
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,10 +16,14 @@ class RefreshTokenService(
     private val getRefreshTokenPort: GetRefreshTokenPort,
     private val deleteRefreshTokenPort: DeleteRefreshTokenPort,
     private val generateTokenUseCase: GenerateTokenUseCase,
+    private val resolveTokenUseCase: ResolveTokenUseCase
 ) : RefreshTokenUseCase {
     @Transactional
-    override suspend fun refresh(securityToken: SecurityToken): SecurityToken {
-        val (userIdentifier, accessToken, refreshToken) = securityToken
+    override suspend fun refresh(accessToken: String, refreshToken: String): SecurityToken {
+        val userIdentifier = resolveTokenUseCase.resolveToken(
+            token = refreshToken,
+            securityTokenType = SecurityTokenType.REFRESH
+        )
         getRefreshTokenPort.getAccessToken(userIdentifier, accessToken, refreshToken)
             ?: throw UnAuthorizedException()
 
