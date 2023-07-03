a7342d950c045935de00596563e65d83a7372da0
dduck00 <ejr075@gmail.com>
2023-07-03 19:14:12 +0900

    del: remove unuse property

--- src/api/src/main/kotlin/com/tenmm/tilserver/post/adapter/inbound/rest/model/ConfirmUploadPostResponse.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/post/adapter/inbound/rest/model/ConfirmUploadPostResponse.kt
@@ -5,16 +5,12 @@ import com.tenmm.tilserver.post.application.inbound.model.PostSaveConfirmResult
 data class ConfirmUploadPostResponse(
     val isSuccess: Boolean,
     val monthlyPublishCount: Int,
-    val month: Int,
-    val year: Int,
 ) {
     companion object {
         fun fromResult(result: PostSaveConfirmResult): ConfirmUploadPostResponse {
             return ConfirmUploadPostResponse(
                 isSuccess = result.operationResult.isSuccess,
                 monthlyPublishCount = result.monthlyPublishCount,
-                month = result.month,
-                year = result.year
             )
         }
     }
