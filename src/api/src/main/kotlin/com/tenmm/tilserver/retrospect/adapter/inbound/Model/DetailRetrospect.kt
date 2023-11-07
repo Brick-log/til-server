20c37b145ce509e18727163eef5e135177bf4f2e
계희준 <gyehuijun@gyehuijun-ui-MacBookPro.local>
2023-11-08 00:27:14 +0900

    modified: lint맞추기

--- src/api/src/main/kotlin/com/tenmm/tilserver/retrospect/adapter/inbound/Model/DetailRetrospect.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/retrospect/adapter/inbound/Model/DetailRetrospect.kt
@@ -2,7 +2,7 @@ package com.tenmm.tilserver.retrospect.adapter.inbound.Model
 
 import java.sql.Timestamp
 
-data class DetailRetrospect (
+data class DetailRetrospect(
     val isSecret: Boolean,
     val id: String,
     val createdAt: Timestamp,
--- src/api/src/main/kotlin/com/tenmm/tilserver/retrospect/adapter/inbound/Model/RetrospectQna.kt
+++ src/api/src/main/kotlin/com/tenmm/tilserver/retrospect/adapter/inbound/Model/RetrospectQna.kt
@@ -1,6 +1,6 @@
 package com.tenmm.tilserver.retrospect.adapter.inbound.Model
 
-data class RetrospectQna (
+data class RetrospectQna(
     val question: String,
     val answer: String,
 )
