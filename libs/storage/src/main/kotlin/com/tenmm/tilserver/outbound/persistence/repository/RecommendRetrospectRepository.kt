c18e3464ad582ab8e241c449091019bff457f5be
계희준 <gyehuijun@gyehuijun-ui-MacBookPro.local>
2023-11-19 17:26:55 +0900

    Modified: 추천 최고 SQL 추가

---
+++ libs/storage/src/main/kotlin/com/tenmm/tilserver/outbound/persistence/repository/RecommendRetrospectRepository.kt
@@ -0,0 +1,19 @@
+package com.tenmm.tilserver.outbound.persistence.repository
+
+import com.tenmm.tilserver.outbound.persistence.entity.RecommendRetrospectEntity
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+
+@Repository
+interface RecommendRetrospectRepository : JpaRepository<RecommendRetrospectEntity, Long> {
+    @Query(value = "SELECT * FROM recommended_retrospect order by RAND() limit :size", nativeQuery = true)
+    fun findByRandom(size: Int): List<RecommendRetrospectEntity>
+
+    fun findByRetrospectTypeOrderByCreatedAtDesc(
+        Type: String,
+        pageable: Pageable,
+    ): List<RecommendRetrospectEntity>
+
+}
